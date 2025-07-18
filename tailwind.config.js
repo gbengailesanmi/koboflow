61fb7188e970e0db8135985be671c25407e29ff3
Gbenga Ilesanmi <ilesanmiolugbenga69@gmail.com>
2025-07-18 20:39:26 +0100

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ['./app/**/*.{ts,tsx}', './components/**/*.{ts,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Roboto', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
