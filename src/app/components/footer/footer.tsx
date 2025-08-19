2fb0ef776a24c3daa12b9c3ce436a2ba9957f33b
Gbenga Ilesanmi <ilesanmiolugbenga69@gmail.com>
2025-08-19 22:12:39 +0100

    Add header

--- src/app/components/footer/footer.tsx
+++ src/app/components/footer/footer.tsx
@@ -19,31 +19,41 @@ export default function Footer() {
 
   return (
     <div className={styles.Footer}>
-      <FooterIconButton onClick={handleHomeClick}>
+      <FooterIconButton onClick={handleHomeClick} text='Home'>
         <HomeIcon width='25' height='25' />
       </FooterIconButton>
-      <FooterIconButton onClick={handleHomeClick}>
+      <FooterIconButton onClick={handleHomeClick} text='Spending'>
         <Pencil2Icon width='25' height='25' />
       </FooterIconButton>
-      <FooterIconButton onClick={handleHomeClick}>
+      <FooterIconButton onClick={handleHomeClick} text='Budget'>
         <MixerHorizontalIcon width='25' height='25' />
       </FooterIconButton>
-      <FooterIconButton onClick={handleHomeClick}>
+      <FooterIconButton onClick={handleHomeClick} text='Deals'>
         <BackpackIcon width='25' height='25' />
       </FooterIconButton>
     </div>
   )
 }
 
-export function FooterIconButton(props: React.ComponentProps<typeof IconButton>) {
+type FooterIconButtonProps = React.ComponentProps<typeof IconButton> & {
+  text?: string
+  children: React.ReactNode
+}
+
+export function FooterIconButton({ text = '', children, ...props }: FooterIconButtonProps) {
   return (
-    <IconButton
-      variant="outline"
-      size="4"
-      color="gray"
-      radius="full"
-      highContrast
-      {...props}
-    />
+    <div className="flex flex-col items-center">
+      <IconButton
+        variant="ghost"
+        size="4"
+        color="gray"
+        radius="full"
+        highContrast
+        {...props}
+      >
+        {children}
+      </IconButton>
+      {text && <div className="text-xs text-center">{text}</div>}
+    </div>
   )
 }
--- src/app/components/header/header.tsx
+++ src/app/components/header/header.tsx
@@ -1,28 +1,49 @@
-import * as Styled from './styles'
-import { Avatar, TextField } from '@radix-ui/themes'
-import { MagnifyingGlassIcon, GearIcon } from '@radix-ui/react-icons'
+import React from 'react'
+import styles from './Header.module.css'
+import { redirect, useParams } from 'next/navigation'
+import { IconButton } from '@radix-ui/themes'
+import { PersonIcon, GearIcon } from '@radix-ui/react-icons'
 
-const Header = () => {
+export default function Header() {
+  const params = useParams()
+  const customerId = params.customerId as string
+
   
+  const handleProfileClick = () => {
+    redirect(`/${customerId}/profileSettings`)
+  }
+
+  // const handlePencilClick = () => {
+
+
+
   return (
-    <Styled.HeaderWrapper>
-      {/* <Styled.AvatarWrapper>
-        <Avatar fallback='G' 
-                src='https://media.licdn.com/dms/image/v2/D4E03AQGKackdOrVWWA/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1693591947730?e=1755734400&v=beta&t=-61IzpZRRGgQ1LSxu1LtkUXnwRZLrbPmBhVpTy6RhzY' 
-                radius='full'
-        />
-      </Styled.AvatarWrapper>
-      <Styled.NameWrapper>
-        <span>Welcome Back, <br /><strong>Gbenga Ilesanmi</strong></span>
-      </Styled.NameWrapper>
-      <TextField.Root placeholder='Search...' radius='full' >
-        <TextField.Slot>
-          <MagnifyingGlassIcon height='16' width='16' />
-        </TextField.Slot>
-      </TextField.Root>
-      <GearIcon height='30' width='26' /> */}
-    </Styled.HeaderWrapper>
+    <div className={styles.HeaderWrapper}>
+      <HeaderIconButton className={styles.SettingsButton} onClick={handleProfileClick}>
+        <PersonIcon width='25' height='25' />
+      </HeaderIconButton>
+      <HeaderIconButton className={styles.SettingsButton} onClick={handleProfileClick}>
+        <GearIcon width='25' height='25' />
+      </HeaderIconButton>
+    </div>
   )
 }
 
-export default Header
+type FooterIconButtonProps = React.ComponentProps<typeof IconButton> & {
+  children: React.ReactNode
+}
+
+export function HeaderIconButton({ children, ...props }: FooterIconButtonProps) {
+  return (
+    <IconButton
+      variant="ghost"
+      size="3"
+      color="gray"
+      radius="full"
+      highContrast
+      {...props}
+    >
+      {children}
+    </IconButton>
+  )
+}
