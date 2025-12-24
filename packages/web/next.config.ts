7ff38839dcdece80bed56217991481821128b80d
Gbenga Ilesanmi <ilesanmiolugbenga69@gmail.com>
2025-12-24 17:49:17 +0000

    un-extend core web vitals

--- packages/web/next.config.ts
+++ packages/web/next.config.ts
@@ -2,7 +2,11 @@ import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ['mongodb', 'bcrypt'],
-  reactStrictMode: false // prevent fetching data twice
+  reactStrictMode: false, // prevent fetching data twice
+
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 }
 
 export default nextConfig
