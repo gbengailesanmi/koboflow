81f1fd7c39cd187d726a03e6ef4677ad9f40b112
Gbenga Ilesanmi <ilesanmiolugbenga69@gmail.com>
2025-12-24 18:50:27 +0000

    yarn

--- packages/web/next.config.ts
+++ packages/web/next.config.ts
@@ -3,10 +3,6 @@ import type { NextConfig } from "next"
 const nextConfig: NextConfig = {
   serverExternalPackages: ['mongodb', 'bcrypt'],
   reactStrictMode: false, // prevent fetching data twice
-
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
 }
 
 export default nextConfig
